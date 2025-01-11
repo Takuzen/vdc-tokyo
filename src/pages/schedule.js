8396a571d768d3e9a58107434bec6bb73bbc0c2a
Takuzen Toh <takuzen0430@gmail.com>
2025-01-11 09:39:30 +0900

    fix: build error

---
+++ src/pages/schedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Press = () => {
+    return (
+        <section id="press" style={{ textAlign: 'center', margin: '50px 0' }}>
+            <h1>報道</h1>
+        </section>
+    );
+};
+
+export default Press;
+
