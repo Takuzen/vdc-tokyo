bcb2ea931c03369715917e7de078ce99c6c60d1d
Takuzen Toh <takuzen0430@gmail.com>
2025-03-24 18:12:46 +0900

    feat: add vercel analytics

--- src/pages/en/index.js
+++ src/pages/en/index.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/react"
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
