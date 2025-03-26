a92225085094b65b72c6d6d39ae57e20b8db1ded
Takuzen Toh <takuzen0430@gmail.com>
2025-03-26 14:09:41 +0900

    fix: Analytics component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,6 +19,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
